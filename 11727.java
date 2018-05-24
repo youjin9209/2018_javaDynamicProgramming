4b76afa909d9689fb1bb6c3fb6466accecc387fe
scarlett <youjin9200@naver.com>
2018-05-24 16:09:56 +0900

    Create 11727.java

---
+++ 11727.java
@@ -0,0 +1,23 @@
+/*
+11727 - 2*n 타일링2 
+
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] d = new int[1001];
+		d[1] = 1;
+		d[2] = 3;
+		for (int i = 3; i <= N; i++) {
+			d[i] = d[i-1] + 2*d[i-2];
+			d[i] = d[i]%10007;
+		}
+		System.out.println(d[N]);
+	}
+}
